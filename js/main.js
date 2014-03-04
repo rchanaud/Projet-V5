d3d4d247e4ef5da9de7c072d0b4ca4437133ffec
rchanaud <remi.chanaud@gmail.com>
2014-03-04 15:50:40 +0100

    Accueil noJS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+(function() {
+	var wineight  = $(window).height();
+	var boxHeight = $("#center-presentation").height();
+	var marginTop = (wineight/2) - (boxHeight/2);
+	$("#center-presentation").css({
+		"margin-top": marginTop
+	});
+}());
