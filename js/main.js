3c369c2bb0c575e492b03392350af61b9141ed32
rchanaud <remi.chanaud@gmail.com>
2014-03-05 15:14:01 +0100

    Animation home beta

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 (function() {
-	var wineight  = $(window).height();
-	var boxHeight = $("#center-presentation").height();
-	var marginTop = (wineight/2) - (boxHeight/2);
+
+	//Init
+	var winHeight  = $(window).height();
+	var winWidth   = $(window).width();
+
+	//Resize SVG
+	$(".svg-shape").width(winWidth);
+	$(".svg-shape").height(winHeight);
+
+	//Centrage vertical
+	var boxHeight  = $("#center-presentation").height();
+	var boxWidth   = $("#center-presentation").width();
+	var marginTop  = (winHeight/2) - (boxHeight/2);
+	var marginLeft = (winWidth/2)  - (boxWidth/2);
 	$("#center-presentation").css({
-		"margin-top": marginTop
+		"top":  marginTop,
+		"left": marginLeft
 	});
+
 }());
