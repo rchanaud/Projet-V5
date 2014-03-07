0390d3218ff8bf34cc1e4a6726b2bfb55b2185d6
rchanaud <remi.chanaud@gmail.com>
2014-03-08 00:04:51 +0100

    Menu beta + espaces projets

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@
 	$(".svg-shape").width(winWidth);
 	$(".svg-shape").height(winHeight);
 
-	//Centrage vertical
+	//Resize every "screen"
+	$(".content-zone").height(winHeight);
+	//TODO: onresize
+
+	//Centrage vertical home
 	var boxHeight  = $("#center-presentation").height();
 	var boxWidth   = $("#center-presentation").width();
 	var marginTop  = (winHeight/2) - (boxHeight/2);
@@ -16,6 +20,25 @@
 	$("#center-presentation").css({
 		"top":  marginTop,
 		"left": marginLeft
+	});
+
+	//Menu
+	$('#reveal-menu').on('click', function(e){
+		e.preventDefault();
+		$menu = $("#main-menu");
+		console.log($menu.height());
+		$menu.slideToggle(
+			function(){
+				$menu.animate({
+					"height": "0"
+				}, 150);
+			},
+			function(){
+				$menu.animate({
+					"height": "100px"
+				}, 150);
+			}
+		);
 	});
 
 }());
